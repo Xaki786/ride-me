f40765acb414a4210968a6053a7e08e6043014c2
Zakria <mjanwarsi@gmail.com>
2019-06-18 12:42:10 +0500

    SERVER STARTING DONE

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+// =============================================================
+// KEYS
+const { mongoURI } = require("./config");
 // =============================================================
 // START MONGO AND SERVER
+// =============================================================
+const PORT = app.get("PORT") || 5000;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MONGO DB STARTED");
+    app.listen(PORT, () => console.log(`SERVER STARTED AT PORT ${PORT}`));
+  })
+  .catch(err => {
+    console.log("Error starting server of mongo");
+  });
 // =============================================================
 // MIDDLEWARES
 // =============================================================
@@ -15,6 +31,8 @@ app.use((req, res, next) => {
   return next(error);
 });
 // =============================================================
+// ERROR HANDLING MIDDLEWARE
+// =============================================================
 app.use((err, req, res, next) => {
   const error = app.get("env") === "development" ? err : {};
   const status = err.status || 500;
@@ -22,6 +40,4 @@ app.use((err, req, res, next) => {
     message: error.message
   });
 });
-// =============================================================
-// ERROR HANDLING MIDDLEWARE
 // =============================================================
