556986343dcb468811a113a6a4357f820403ffed
Muhammad Zakria Jan <mjanwarsi@gmail.com>
2019-09-01 23:26:40 +0500

    updateuser route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const router = require("express-promise-router")({ mergeParams: true });
 // ==============================================================
 // IMPORTING USER CONTROLLER METHODS
 // ==============================================================
-const { createUser, getOneUser } = require("../controllers").userController;
+const { createUser, getOneUser,updateUser } = require("../controllers").userController;
 // ====================================================================
 // @route   => '/api/users/'
 // @desc    => Create new user
@@ -12,5 +12,12 @@ router.route("/register").post(createUser);
 // @route   => /api/users/:userId
 // @desc    => Show specific user
 router.route("/:userId").get(getOneUser);
+router.route("/:userId").put(updateUser);
 // ====================================================================
 module.exports = router;
+
+Latest commit
+43a0cd4
+now
+Type 	Name 	Latest commit message 	Commit time
+	..
